5d4205498cf746fda007dbc847dd1ec3ecfa1f04
1revolman1 <1revolman1@gmail.com>
2022-11-30 15:05:14 +0200

    Create sign-in example

--- dapper-wallet-storefront/src/pages/_app.tsx
+++ dapper-wallet-storefront/src/pages/_app.tsx
@@ -1,25 +1,29 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import { AppProps as NextAppProps } from "next/app"
+import { SessionProvider } from "next-auth/react"
 import { WalletProvider } from "../components/wallet/WalletProvider"
 import { ComponentWithWallet } from "../lib/types"
 import "../styles/global.css"
 
 import theme from "../lib/chakra-theme"
 import { GraphQLClientProvider } from "../lib/GraphQLClientProvider"
+import { Session } from "next-auth"
 
-type AppProps<P = {}> = NextAppProps<P> & {
+type AppProps<P = { session: Session }> = NextAppProps<P> & {
   Component: ComponentWithWallet
 }
 
-const App = ({ Component, pageProps: { ...pageProps } }: AppProps): JSX.Element => {
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps): JSX.Element => {
   return (
-    <WalletProvider requireWallet={Component.requireWallet}>
-      <GraphQLClientProvider>
-        <ChakraProvider theme={theme}>
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </GraphQLClientProvider>
-    </WalletProvider>
+    <SessionProvider session={session}>
+      <WalletProvider requireWallet={Component.requireWallet}>
+        <GraphQLClientProvider>
+          <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+          </ChakraProvider>
+        </GraphQLClientProvider>
+      </WalletProvider>
+    </SessionProvider>
   )
 }
 
--- dapper-wallet-storefront/src/pages/app/account.tsx
+++ dapper-wallet-storefront/src/pages/app/account.tsx
@@ -1,17 +1,41 @@
 import { Divider, VStack } from "@chakra-ui/react"
-
-import AppLayout from "../../components/AppLayout"
-import { LogOut } from "../../components/LogOut"
-import { WalletSetup } from "../../components/wallet/WalletSetup"
-import { SectionHeader } from "../../ui/SectionHeader"
+import { useSession, signIn, signOut } from "next-auth/react"
+import AppLayout from "src/components/AppLayout"
+import { LogOut } from "src/components/LogOut"
+import { WalletSetup } from "src/components/wallet/WalletSetup"
+import { SectionHeader } from "src/ui/SectionHeader"
+import { Button } from "@chakra-ui/react"
 
 const AccountPage = () => {
+  const { data: session } = useSession()
+
   return (
     <AppLayout>
       <VStack>
         <SectionHeader text="My Account" />
         <WalletSetup />
         <Divider w="80%" maxW="xl" py="8" />
+        <Button
+          onClick={() => {
+            console.log("click", session)
+          }}
+        >
+          Get Session
+        </Button>
+        <Button
+          onClick={() => {
+            signIn()
+          }}
+        >
+          Sign In
+        </Button>
+        <Button
+          onClick={() => {
+            signOut()
+          }}
+        >
+          Sign Out
+        </Button>
         <LogOut />
       </VStack>
     </AppLayout>
