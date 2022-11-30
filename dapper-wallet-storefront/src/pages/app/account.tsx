72792a7c6f6c4b4404ea84a785356e58f3660c2d
1revolman1 <1revolman1@gmail.com>
2022-11-30 19:26:52 +0200

    Update account page

--- dapper-wallet-storefront/src/pages/app/account.tsx
+++ dapper-wallet-storefront/src/pages/app/account.tsx
@@ -1,14 +1,28 @@
-import { Divider, VStack } from "@chakra-ui/react"
-import { useSession, signIn, signOut } from "next-auth/react"
+import { Button, Divider, VStack } from "@chakra-ui/react"
+import { signIn, signOut, useSession } from "next-auth/react"
 import AppLayout from "src/components/AppLayout"
 import { LogOut } from "src/components/LogOut"
 import { WalletSetup } from "src/components/wallet/WalletSetup"
 import { SectionHeader } from "src/ui/SectionHeader"
-import { Button } from "@chakra-ui/react"
+
+// export const getStaticProps: GetStaticProps = async () => {
+//   const feed = await prisma.user.findMany({
+//     // where: {  },
+//     // include: {
+//     //   author: {
+//     //     select: { name: true },
+//     //   },
+//     // },
+//   })
+//   console.log(feed)
+//   return {
+//     props: { feed },
+//     revalidate: 10,
+//   }
+// }
 
 const AccountPage = () => {
   const { data: session } = useSession()
-
   return (
     <AppLayout>
       <VStack>
