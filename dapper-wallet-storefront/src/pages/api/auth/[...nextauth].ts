47be72c231536294f59ab69311bcc62d169ba637
1revolman1 <1revolman1@gmail.com>
2022-12-01 15:19:56 +0200

    Add wallet instance to user

--- dapper-wallet-storefront/src/pages/api/auth/[...nextauth].ts
+++ dapper-wallet-storefront/src/pages/api/auth/[...nextauth].ts
@@ -17,5 +17,23 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: "/app/sign-in",
   },
+  callbacks: {
+    session: async ({ session, token, user }) => {
+      const userFromDB = await prisma.user.findUnique({
+        where: { email: session.user.email },
+        include: { wallet: true },
+      })
+
+      return {
+        user: {
+          name: session?.user?.name,
+          image: session?.user?.image,
+          email: session?.user?.email,
+          walletAddress: userFromDB.wallet.address,
+        },
+        expires: session?.expires,
+      }
+    },
+  },
   adapter: PrismaAdapter(prisma),
 }
