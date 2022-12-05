50a4dc4b249b8559bf454453923b5f549585e081
1revolman1 <1revolman1@gmail.com>
2022-12-05 13:11:10 +0200

    Update document account

---
+++ dapper-wallet-storefront/src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+      {/* <script
+        async
+        type="text/javascript"
+        src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=WbaT6T"
+      ></script> */}
+    </Html>
+  )
+}
