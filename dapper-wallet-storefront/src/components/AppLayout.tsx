138778c6cf975bbaae57222bd5cb27106ccf8cbe
1revolman1 <1revolman1@gmail.com>
2022-12-14 18:39:35 +0200

    Remove missing parts

--- dapper-wallet-storefront/src/components/AppLayout.tsx
+++ dapper-wallet-storefront/src/components/AppLayout.tsx
@@ -1,5 +1,3 @@
-import { Box, Flex } from "@chakra-ui/layout"
-
 import Footer from "../ui/Footer"
 import { Navbar } from "../ui/Navbar/Nav"
 
@@ -10,18 +8,11 @@ type Props = {
 export default function AppLayout({ children }: Props) {
   return (
     <main>
-      {/* <Flex direction="column" minH="100vh" minW="320"> */}
       <Navbar />
       <section className="pt-20 min-h-[calc(100vh-theme(space.20))] flex">
         <div className="mx-auto">{children}</div>
       </section>
-      {/* <Box bg="page.background" flexGrow={1}>
-        <Box w="100%" py="12">
-          {children}
-        </Box>
-      </Box> */}
       <Footer />
-      {/* </Flex> */}
     </main>
   )
 }
