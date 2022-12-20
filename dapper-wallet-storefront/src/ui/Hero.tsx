5f39038b6a409e4f4ff46c1b336447f4962595a6
1revolman1 <1revolman1@gmail.com>
2022-12-20 18:17:13 +0200

    Update hero container

--- dapper-wallet-storefront/src/ui/Hero.tsx
+++ dapper-wallet-storefront/src/ui/Hero.tsx
@@ -1,17 +1,37 @@
 import React, { memo } from "react"
 import Image from "next/image"
+import ScrollDown from "src/icon/ScrollDown.svg"
 
 function Hero() {
   return (
-    <section className="container bg-main flex flex-col gap-4 md:flex-row md:items-center px-2">
-      <div className="text-white font-bangers">
-        <h1 className="uppercase text-4xl md:text-8xl mb-2 md:mb-8">placeholder</h1>
-        <p className="text-2xl md:text-4xl">
-          Lacus ultricies nascetur fermentum augue fermentum viverra.
-        </p>
-      </div>
-      <div>
-        <Image src="/braintrain.png" alt="BrainTrain main image" width={640} height={690} />
+    <section className="relative -top-16 w-full bg-main flex flex-col gap-4 md:flex-row md:items-center px-2">
+      <section className="min-h-screen ">
+        <Image
+          src="/BG.jpg"
+          alt="BrainTrain main image"
+          fill
+          style={{
+            objectFit: "cover",
+            minHeight: "100vh",
+          }}
+          sizes="100vw"
+        />
+      </section>
+      <div className="relative container grid grid-cols-[521px_1fr] gap-5">
+        <section className="z-10">
+          {/* <img src="/product.png" alt="" /> */}
+          <Image src="/product.png" alt="BrainTrain Product" width={526} height={498} />
+        </section>
+        <div className="text-white font-bangers max-w-lg flex flex-col justify-center">
+          <h1 className="uppercase leading-[61px] text-[56px] mb-2 md:mb-6">BRAIN TRAIN TICKETS</h1>
+          <p className="font-dosis text-lg">
+            <strong className="mb-6 flex font-bold text-xl">
+              The Brain Train is the premier ride to the Gamisodes Inspector Gadget metaverse
+            </strong>
+            Drawn by celebrity artist Agnes Garowska, each Brain Train Ticket depicts Brain wearing
+            a unique costume using elements featured in Season 1 of the 1980s animated series.
+          </p>
+        </div>
       </div>
     </section>
   )
