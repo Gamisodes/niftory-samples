1b8407ddf7db68513ccc99dcce308acb59a4d7db
1revolman1 <1revolman1@gmail.com>
2022-12-28 20:09:01 +0200

    Update to id

--- dapper-wallet-storefront/src/pages/api/users/[id].ts
+++ dapper-wallet-storefront/src/pages/api/users/[id].ts
@@ -2,10 +2,10 @@ import { NextApiHandler, NextApiRequest, NextApiResponse } from "next"
 import prisma from "src/lib/prisma"
 
 const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== "GET") {
+  if (!(req.method === "GET" || req.method === "DELETE")) {
     res.status(405).json({
       success: false,
-      errors: ["Method not allowed, this endpoint only supports GET"],
+      errors: ["Method not allowed, this endpoint only supports GET or DELETE"],
     })
     return
   }
@@ -13,22 +13,47 @@ const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse
   if (!id) {
     res.status(401).json({ errors: ["Must have id."], success: false })
   }
-  try {
-    const wallet = await prisma.wallet.findMany({
-      where: {
-        address: id,
-      },
-      include: {
-        user: true,
-      },
-    })
-    res.status(200).json({ success: true, data: wallet ?? [] })
-  } catch (error) {
-    res.status(500).json({
-      error: [error],
-      success: false,
-    })
+  if (req.method === "GET")
+    try {
+      const wallet = await prisma.wallet.findUnique({
+        where: {
+          address: id,
+        },
+        include: {
+          user: true,
+        },
+      })
+      res.status(200).json({ success: true, data: wallet ?? [] })
+      return
+    } catch (error) {
+      res.status(500).json({
+        error: [error],
+        success: false,
+      })
+    }
+  if (req.method === "DELETE") {
+    try {
+      const wallet = await prisma.wallet.findUnique({
+        where: {
+          address: id,
+        },
+        include: {
+          user: true,
+        },
+      })
+      if (wallet) await prisma.wallet.delete({ where: { id: wallet.id } })
+      res.status(200).json({ success: true, data: wallet ?? [] })
+      return
+    } catch (error) {
+      res.status(500).json({
+        error: [error],
+        success: false,
+      })
+    }
   }
+  res.status(500).json({
+    success: false,
+  })
 }
 
 export default handler
