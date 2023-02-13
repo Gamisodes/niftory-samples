7090ffed0f881ecc7864df5546b5367932b914f0
Nikita Gritsenko <nikita.gritsenko@urich.org>
2023-02-13 16:29:46 +0000

    Gs 39 update header

--- dapper-wallet-storefront/src/ui/Navbar/Nav.tsx
+++ dapper-wallet-storefront/src/ui/Navbar/Nav.tsx
@@ -7,17 +7,55 @@ export const Navbar = () => {
     return [
       [
         {
-          title: "Buy Ticket",
-          href: "/app/drops/" + process.env.NEXT_PUBLIC_DROP_ID,
+          title: "Store",
+          href: "https://gamisodes.com/collections",
         },
         {
-          title: "My Collection",
-          href: "/app/collection",
+          title: "Collections",
+          href: "https://gamisodes.com/pages/collections",
+        },
+        {
+          title: "About",
+          href: "/",
+          submenu: [
+            {
+              title: 'Team',
+              href: 'https://gamisodes.com/pages/team-1'
+            },
+            {
+              title: 'FAQ',
+              href: 'https://gamisodes.com/pages/faqs'
+            },
+            {
+              title: 'Blog',
+              href: 'https://gamisodes.com/blogs/news'
+            },
+          ]
         },
         {
           title: "My Account",
-          href: "/app/account",
+          href: "/",
+          submenu: [
+            {
+              title: 'Sign in / Sign up',
+              href: '/app/account'
+            },
+            {
+              title: 'My Brain & Paris Cards',
+              href: '/app/collection'
+            },
+            {
+              title: 'My VIP & Trading Cards',
+              href: 'https://gamisodes.mint.store',
+              newTab: true
+            },
+          ]
         },
+        {
+          title: "Cart",
+          href: "/app/drops/" + process.env.NEXT_PUBLIC_DROP_ID,
+          hideOnMobile: true
+        }
       ],
     ]
   }, [])
