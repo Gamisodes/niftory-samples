f00bf44e724336b6dd87d212c36436951843f5e4
Nikita <nikita.gritsenko@urich.org>
2023-02-21 16:32:37 +0200

    GS-37 add more check

--- dapper-wallet-storefront/src/hooks/useCollectionMainInterface.ts
+++ dapper-wallet-storefront/src/hooks/useCollectionMainInterface.ts
@@ -14,8 +14,7 @@ export function useCollectionMainInterface(
   >
 ) {
   const brainTrainCollection = useMemo(() => {
-    const nftsList = nftsByWalletResponse?.data?.nftsByWallet?.items
-      .filter((nft) =>
+    const nftsList = nftsByWalletResponse?.data?.nftsByWallet?.items?.filter((nft) =>
         [NftBlockchainState.Transferred, NftBlockchainState.Transferring].includes(
           nft.blockchainState
         )
@@ -50,7 +49,7 @@ export function useCollectionMainInterface(
       VIPCollection: {},
     }
 
-    const collectionWithInterface = gamisodesCollections?.items.map((nft) => ({
+    const collectionWithInterface = gamisodesCollections?.items?.map((nft) => ({
       ...nft,
       imageUrl: nft.display.thumbnail.url,
       title: nft.display.name,
