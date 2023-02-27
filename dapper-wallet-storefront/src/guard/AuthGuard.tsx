bcaef77526b1fd04fe1cef5aa94c83c5063ae926
1revolman1 <1revolman1@gmail.com>
2023-02-27 20:09:28 +0200

    Return metags

--- dapper-wallet-storefront/src/guard/AuthGuard.tsx
+++ dapper-wallet-storefront/src/guard/AuthGuard.tsx
@@ -2,6 +2,7 @@ import { useSession, signIn } from "next-auth/react"
 import Link from "next/link"
 import { PropsWithChildren } from "react"
 import AppLayout from "src/components/AppLayout"
+import { MetaTags } from "src/components/general/MetaTags"
 import { SectionHeader } from "src/ui/SectionHeader"
 
 function AuthGuard({ children }: PropsWithChildren) {
@@ -12,36 +13,42 @@ function AuthGuard({ children }: PropsWithChildren) {
 
   if (status === "unauthenticated") {
     return (
-      <AppLayout>
-        <section className="mx-auto text-black">
-          <SectionHeader text="Access denied" />
-          <div className="flex flex-col justify-center items-center">
-            <p className="text-dosis text-black text-xl font-bold pb-6">
-              You need to login to our app
-            </p>
-            {/* <button
+      <>
+        <MetaTags />
+        <AppLayout>
+          <section className="mx-auto text-black">
+            <SectionHeader text="Access denied" />
+            <div className="flex flex-col justify-center items-center">
+              <p className="text-dosis text-black text-xl font-bold pb-6">
+                You need to login to our app
+              </p>
+              {/* <button
               onClick={() => signIn()}
               className="uppercase font-dosis font-bold text-base p-2 px-16 text-white transition-colors bg-header hover:bg-purple"
             >
               Sign in / Sign up
             </button> */}
-            <Link href={"/app/sign-in"} className="flex w-fit items-center">
-              <button className="uppercase font-dosis font-bold text-base p-2 px-16 text-white transition-colors bg-header hover:bg-purple">
-                Sign in / Sign up
-              </button>
-            </Link>
-          </div>
-        </section>
-      </AppLayout>
+              <Link href={"/app/sign-in"} className="flex w-fit items-center">
+                <button className="uppercase font-dosis font-bold text-base p-2 px-16 text-white transition-colors bg-header hover:bg-purple">
+                  Sign in / Sign up
+                </button>
+              </Link>
+            </div>
+          </section>
+        </AppLayout>
+      </>
     )
   }
 
   return (
-    <AppLayout>
-      <section className="mx-auto text-black">
-        <SectionHeader text="Loading..." />
-      </section>
-    </AppLayout>
+    <>
+      <MetaTags />
+      <AppLayout>
+        <section className="mx-auto text-black">
+          <SectionHeader text="Loading..." />
+        </section>
+      </AppLayout>
+    </>
   )
 }
 
--- dapper-wallet-storefront/src/guard/WalletGuard.tsx
+++ dapper-wallet-storefront/src/guard/WalletGuard.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import { PropsWithChildren } from "react"
 import AppLayout from "src/components/AppLayout"
+import { MetaTags } from "src/components/general/MetaTags"
 import { useWalletContext } from "src/hooks/useWalletContext"
 import { SectionHeader } from "src/ui/SectionHeader"
 
@@ -9,21 +10,24 @@ function WalletGuard({ children }: PropsWithChildren) {
 
   if (currentUser && !currentUser.loggedIn) {
     return (
-      <AppLayout>
-        <section className="mx-auto text-black">
-          <SectionHeader text="Access denied" />
-          <div className="flex flex-col justify-center items-center">
-            <p className="text-dosis text-black text-xl font-bold pb-6">
-              You need to login to your Dapper Wallet
-            </p>
-            <Link href={"/app/account"} className="flex w-fit items-center">
-              <button className="uppercase font-dosis font-bold text-base p-2 px-16 text-white transition-colors bg-header hover:bg-purple">
-                Sign in / Sign up
-              </button>
-            </Link>
-          </div>
-        </section>
-      </AppLayout>
+      <>
+        <MetaTags />
+        <AppLayout>
+          <section className="mx-auto text-black">
+            <SectionHeader text="Access denied" />
+            <div className="flex flex-col justify-center items-center">
+              <p className="text-dosis text-black text-xl font-bold pb-6">
+                You need to login to your Dapper Wallet
+              </p>
+              <Link href={"/app/account"} className="flex w-fit items-center">
+                <button className="uppercase font-dosis font-bold text-base p-2 px-16 text-white transition-colors bg-header hover:bg-purple">
+                  Sign in / Sign up
+                </button>
+              </Link>
+            </div>
+          </section>
+        </AppLayout>
+      </>
     )
   }
 
@@ -32,11 +36,14 @@ function WalletGuard({ children }: PropsWithChildren) {
   }
 
   return (
-    <AppLayout>
-      <section className="mx-auto text-black">
-        <SectionHeader text="Loading..." />
-      </section>
-    </AppLayout>
+    <>
+      <MetaTags />
+      <AppLayout>
+        <section className="mx-auto text-black">
+          <SectionHeader text="Loading..." />
+        </section>
+      </AppLayout>
+    </>
   )
 }
 
