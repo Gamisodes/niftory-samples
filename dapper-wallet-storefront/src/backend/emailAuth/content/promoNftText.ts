8c420c5084d4d237140849760b8251d27610080a
1revolman1 <1revolman1@gmail.com>
2023-07-26 12:07:15 +0300

    Update promo NFT text

--- dapper-wallet-storefront/src/backend/emailAuth/content/promoNftText.ts
+++ dapper-wallet-storefront/src/backend/emailAuth/content/promoNftText.ts
@@ -2,5 +2,5 @@ export type ITextAnswerParams = { url: string; host: string }
 
 /** Email Text body (fallback for email clients that don't render HTML, e.g. feature phones) */
 export function promoNftText({ url, host }: ITextAnswerParams) {
-  return `Get your free NFT in ${host}\n${url}\n\n`
+  return `Get your free Digital Collectible at ${host}\n${url}\n\n`
 }
